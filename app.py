6354853e07bd3438e65ed8bf4a3ec05792e008c8
VoyagerXyroo <124270543+VoyagerXyroo@users.noreply.github.com>
2024-07-24 21:12:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, request, render_template, send_file, make_response
+from flask import Flask, request, render_template, send_file
 import requests
-import os
+from io import BytesIO
 
 app = Flask(__name__)
 
-# Ganti dengan API key DocRaptor Anda
 DOC_RAPTOR_API_KEY = 'mC5OrJ9rmtDrE-c3pOC6'
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,34 +11,22 @@ def index():
     if request.method == 'POST':
         url = request.form['url']
         pdf_name = request.form['pdf_name']
-        
+
         if not pdf_name or any(c in pdf_name for c in ['/', '\\', ':', '*', '?', '"', '<', '>', '|']):
             return "Invalid PDF name. Please avoid using special characters."
-
-        pdf_path = os.path.join(os.getcwd(), f'{pdf_name}.pdf')
 
         try:
             response = requests.post(
                 'https://docraptor.com/docs',
                 auth=(DOC_RAPTOR_API_KEY, ''),
-                json={
-                    'document_url': url,
-                    'name': pdf_path,
-                    'test': True
-                },
+                json={'document_url': url, 'test': True},
                 headers={'Content-Type': 'application/json'},
                 stream=True
             )
-
+            
             if response.status_code == 200:
-                # Menulis PDF ke file sistem
-                with open(pdf_path, 'wb') as f:
-                    f.write(response.content)
-
-                # Mengirimkan file untuk diunduh oleh pengguna
-                resp = make_response(send_file(pdf_path, as_attachment=True))
-                resp.headers["Content-Disposition"] = f"attachment; filename={pdf_name}.pdf"
-                return resp
+                pdf_file = BytesIO(response.content)
+                return send_file(pdf_file, as_attachment=True, download_name=f'{pdf_name}.pdf')
             else:
                 return f"An error occurred: {response.text}"
         except Exception as e:
